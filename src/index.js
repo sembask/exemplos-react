0f4beb78de0839daba1885745453122d062e8436
Lucas Sembarski Higert <lucassembarskihigert@gmail.com>
2020-01-27 11:28:14 -0300

    Maneiras de retornar multi elementos

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
 // import ComponenteProps from './componentes/ComponenteProps'
-import {CompA, compB, CompB} from './componentes/DoisComponentes'
+// import {CompA, compB, CompB} from './componentes/DoisComponentes'
+import MultiElementos from './componentes/MultiElementos';
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
 <div>
-<CompA  valor={2*20}/>
-<CompB  valor = {2*10}/>
+    <MultiElementos/>
 </div>, elemento)
