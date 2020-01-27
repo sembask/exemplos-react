d9cf2b2f85771b015031e39a03c291a7415d0758
Lucas Sembarski Higert <lucassembarskihigert@gmail.com>
2020-01-27 10:45:17 -0300

    Trabalhando com dois componentes em um arquivo jsx

--- src/componentes/PrimeiroComponente.jsx
+++ src/componentes/PrimeiroComponente.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 //Componente baseado em função
-export default function (){
+ function primeiro (){
     return <h1>Primeiro Componente</h1>
 }
+export default primeiro;
+
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PrimeiroComponente from './componentes/PrimeiroComponente';
+// import PrimeiroComponente from './componentes/PrimeiroComponente'
+// import ComponenteProps from './componentes/ComponenteProps'
+import {CompA, compB, CompB} from './componentes/DoisComponentes'
 
 const elemento = document.getElementById('root')
-ReactDOM.render(<div><PrimeiroComponente/></div>, elemento)
+ReactDOM.render(
+<div>
+<CompA  valor={2*20}/>
+<CompB  valor = {2*10}/>
+</div>, elemento)
