eff80d088b407d1c2dbb37fbd01572a01c8532f9
Lucas Sembarski Higert <lucassembarskihigert@gmail.com>
2020-01-27 14:19:35 -0300

    Passando as propriedas de um pai para um filho

---
+++ src/componentes/Familia.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {filhosComProps} from '../utils/utils'
+export default props =>
+<div>
+    <h1>Familia</h1>
+ {filhosComProps(props)}
+</div>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
 // import ComponenteProps from './componentes/ComponenteProps'
-import CompA, {CompB} from './componentes/DoisComponentes'
+// import CompA, {CompB} from './componentes/DoisComponentes'
 // import MultiElementos from './componentes/MultiElementos';
+// import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
 <div>
-    <CompA/>
-    <CompB/>
+    <Familia sobrenome=" Pereira" >
+        <Membro nome="Andre" />
+        <Membro nome="Mariana" />
+    </Familia>
+    <Familia sobrenome=" Arruda">
+        <Membro nome="Bia" />
+        <Membro nome="Gustavo"/>
+    </Familia>
 </div>, elemento)
